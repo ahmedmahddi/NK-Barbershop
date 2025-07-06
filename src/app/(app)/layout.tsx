092225bc4e9df3f03a0ebdfd4f1d431f9c490c65
ahmedmahddi <155826291+ahmedmahddi@users.noreply.github.com>
2025-07-06 11:56:00 +0100

    Update layout.tsx

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -8,10 +8,10 @@ import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
-  title: "Salon Naim Kchao | Coiffures et Soins à Sfax",
+  title: "Naim Kchao Barbershop| Coiffures et Soins à Sfax",
   icons: "/images/LogoNK.png",
   description:
-    "Visitez le Salon Naim Kchao à Sfax pour des coiffures expertes, des tailles de barbe et des services de soin haut de gamme. Réservez votre rendez-vous aujourd'hui !",
+    "Visitez le Naim Kchao Barbershop à Sfax pour des coiffures expertes, des tailles de barbe et des services de soin haut de gamme. Réservez votre rendez-vous aujourd'hui !",
   keywords: [
     "salon de coiffure",
     "coiffures",
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Salon Naim Kchao | Services de Soin à Sfax",
     description:
-      "Coiffures et soins haut de gamme au Salon Naim Kchao à Sfax, Tunisie. Réservez maintenant pour un look frais !",
+      "Coiffures et soins haut de gamme au Naim Kchao Barbershop à Sfax, Tunisie. Réservez maintenant pour un look frais !",
     url: "https://www.naimkchaobarbershop.com",
     siteName: "Naim Kchao Barbershop",
   },
